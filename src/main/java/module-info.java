9f11cdf54848c3b707c14073e64f932a7a740bc0
JubaOuabou <aouabou@ies-eugeni.cat>
2024-05-02 20:33:03 +0200

    module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,18 @@
 module daiana.abde.projecteabdedaiana {
     requires javafx.controls;
     requires javafx.fxml;
+    //requires Fitxers;
+    requires com.google.zxing;
+    requires com.google.zxing.javase;
+    requires java.desktop;
+
 
 
     opens daiana.abde.projecteabdedaiana to javafx.fxml;
     exports daiana.abde.projecteabdedaiana;
     opens daiana.abde.projecteabdedaiana.Controllers to javafx.fxml;
     exports daiana.abde.projecteabdedaiana.Controllers;
+
+    exports daiana.abde.projecteabdedaiana.Classes;
+    opens daiana.abde.projecteabdedaiana.Classes to javafx.fxml;
 }
