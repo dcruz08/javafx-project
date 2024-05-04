8441ded323dda86fb1212cc2f69603e2ccfbab1b
JubaOuabou <aouabou@ies-eugeni.cat>
2024-05-04 21:57:01 +0200

    usuario-normal.fxml 04-05-2024

--- src/main/java/daiana/abde/projecteabdedaiana/HelloApplication.java
+++ src/main/java/daiana/abde/projecteabdedaiana/HelloApplication.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("usuario-normal.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Projecte | Grup 10 | Daiana | Abdel");
         stage.setScene(scene);
